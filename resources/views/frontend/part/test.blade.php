d8c158e46431d9ffe72a7c219eb330b96ef57504
Le Tuan Anh <anhlt2@vnext.com.vn>
2020-09-29 16:30:01 +0700

    Trắc nghiệm giữa khóa học cho học sinh

---
+++ resources/views/frontend/part/test.blade.php
@@ -0,0 +1,100 @@
+@extends('frontend.part.master')
+
+@section('content')
+<div class="learningLesson__quiz" id="test">
+    <div class="quiz-wrap">
+        <div class="quiz-wrap__header">
+            <div class="header-left">
+                <p>Họ và tên <span>{{ auth()->user()->name ?? auth()->user()->username }}</span></p>
+            </div>
+            <div class="header-right">
+                <h3 class="header-subtitle">Trung tâm ngoại ngữ Tomato</h3>
+                <h2 class="header-title">Kiểm tra hiểu bài</h2>
+            </div>
+        </div>
+        <div class="quiz-wrap__inner" :class="{ 'pointerEventsNone': submited }">
+            <div class="quiz-wrap__content">
+                <ul class="quiz__list">
+                    <li v-for="(question, questionIndex) in questions" class="item">
+                        <div class="item__title">
+                            <p><b>Câu hỏi số @{{ questionIndex + 1 }}:</b> @{{ question.question }}</p>
+                        </div>
+                        <div class="item__choose">
+                            <label v-for="(option, optionIndex) in question.options" class="choose-label" :class="{ 'true': submited && question.correct == option }">
+                                <input type="radio" v-model="question.selected" :value="option">
+                                <div class="choose-label__inner">
+                                    <span class="choose-label__check">@{{ String.fromCharCode(65 + optionIndex) }}</span>
+                                    <p>@{{ option }}</p>
+                                </div>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+
+                <div class="quiz-wrap__footer">
+                    <button type="button" class="btn" @click="submit" :class="{ 'd-none': submited }">Nộp bài</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="quiz-reslut">
+        <div class="quiz-reslut__inner">
+            <div class="quiz-reslut__content">
+                <div class="quiz-reslut__right">
+                    <ul class="quiz-reslut__list">
+                        <li>Họ và tên: <b>{{ auth()->user()->name ?? auth()->user()->username }}</b></li>
+                        <li>Bài thi: <b>{{ $part->title }}</b></li>
+                        <li>Câu hỏi <b>@{{ questions.filter(x => x.selected == x.correct).length }}/@{{ questions.length }}</b></li>
+                        <li>Tổng điểm: <b>@{{ Math.round(questions.filter(x => x.selected == x.correct).length / questions.length * 100) / 10 }}/10</b></li>
+                        <li>Kết quả: <b>Chưa đạt</b></li>
+                    </ul>
+                    <div class="quiz-reslut__btn text-left">
+                        <button type="button" class="btn" @click="scrollToQuiz">Xem đáp án</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('footer')
+<script>
+    new Vue({
+        el: '#test',
+        data: {
+            questions: [],
+            submited: false,
+        },
+        mounted() {
+            this.questions = JSON.parse(`{!! json_encode($data->data) !!}`);
+            this.questions = this.questions.map(question => {
+                question.correct = question.options[question.correct];
+                question.options = this.shuffle(question.options);
+                return question;
+            });
+            this.questions = this.shuffle(this.questions);
+        },
+        methods: {
+            shuffle(arr) {
+                return arr.sort(() => Math.random() - 0.5);
+            },
+            submit() {
+                this.submited = true;
+
+                $('.quiz-reslut').slideDown();
+                $('html,body').animate({
+                    scrollTop: $('.quiz-reslut').offset().top
+                }, 500);
+            },
+            scrollToQuiz() {
+                $('html,body').animate({
+                    scrollTop: $('.quiz-wrap').offset().top
+                }, 500);
+            },
+        },
+    });
+
+</script>
+@endsection
