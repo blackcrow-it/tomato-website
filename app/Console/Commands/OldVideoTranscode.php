2fae29e674ac101a34bb6ae2709254d3303bccdc
mrcyclo <vipboysanhdieu@gmail.com>
2020-12-08 20:18:44 +0700

    Hỗ trợ nhiều luồng transcode

--- app/Console/Commands/OldVideoTranscode.php
+++ app/Console/Commands/OldVideoTranscode.php
@@ -87,112 +87,139 @@ class OldVideoTranscode extends Command
 
         printf("Get videos data.\n");
 
-        $videos = DB::connection('old_db')->table('video')->where('drive_id', '!=', '')->whereNull('stream_url')->get();
+        while (true) {
+            $video = DB::connection('old_db')
+                ->table('video')
+                ->where('drive_id', '!=', '')
+                ->whereNull('stream_url')
+                // ->orderBy('video_id', 'desc')
+                ->first();
 
-        foreach ($videos as $video) {
-            $client->fetchAccessTokenWithRefreshToken($refreshToken);
+            if ($video == null) break;
 
-            $mp4Path = 'transcode/' . $video->video_id . '.mp4';
-            $keyPath = 'transcode/secret.key';
-            $m3u8Path = 'transcode/hls/playlist.m3u8';
+            try {
+                printf("Video: " . $video->title . ".\n");
 
-            printf("Download video: " . $video->title . ".\n");
+                printf("Set current trancode.\n");
 
-            $file = $drive->files->get($video->drive_id, [
-                'fields' => 'id, size'
-            ]);
+                DB::connection('old_db')->table('video')->where('video_id', $video->video_id)->update([
+                    'stream_url' => 0
+                ]);
 
-            if (!Storage::exists($mp4Path) || Storage::size($mp4Path) != $file->getSize()) {
-                printf("Re-create transcode directory.\n");
-                Storage::deleteDirectory('transcode');
-                Storage::makeDirectory('transcode');
+                $client->fetchAccessTokenWithRefreshToken($refreshToken);
 
-                // Get the authorized Guzzle HTTP client
-                $http = $client->authorize();
+                $mp4Path = 'transcode/' . $video->video_id . '.mp4';
+                $keyPath = 'transcode/secret.key';
+                $m3u8Path = 'transcode/hls/playlist.m3u8';
 
-                // Open a file for writing
-                $fp = fopen(Storage::path($mp4Path), 'w');
+                printf("Download video.\n");
 
-                // Download in 10 MB chunks
-                $chunkSizeBytes = 10 * 1024 * 1024;
-                $chunkStart = 0;
+                $file = $drive->files->get($video->drive_id, [
+                    'fields' => 'id, size'
+                ]);
 
-                // Iterate over each chunk and write it to our file
-                while ($chunkStart < $file->getSize()) {
-                    $chunkEnd = $chunkStart + $chunkSizeBytes;
-                    $response = $http->request(
-                        'GET',
-                        sprintf('/drive/v3/files/%s', $file->getId()),
-                        [
-                            'query' => ['alt' => 'media'],
-                            'headers' => [
-                                'Range' => sprintf('bytes=%s-%s', $chunkStart, $chunkEnd)
+                if (!Storage::exists($mp4Path) || Storage::size($mp4Path) != $file->getSize()) {
+                    printf("Re-create transcode directory.\n");
+                    Storage::deleteDirectory('transcode');
+                    Storage::makeDirectory('transcode');
+
+                    // Get the authorized Guzzle HTTP client
+                    $http = $client->authorize();
+
+                    // Open a file for writing
+                    $fp = fopen(Storage::path($mp4Path), 'w');
+
+                    // Download in 10 MB chunks
+                    $chunkSizeBytes = 10 * 1024 * 1024;
+                    $chunkStart = 0;
+
+                    // Iterate over each chunk and write it to our file
+                    while ($chunkStart < $file->getSize()) {
+                        $chunkEnd = $chunkStart + $chunkSizeBytes;
+                        $response = $http->request(
+                            'GET',
+                            sprintf('/drive/v3/files/%s', $file->getId()),
+                            [
+                                'query' => ['alt' => 'media'],
+                                'headers' => [
+                                    'Range' => sprintf('bytes=%s-%s', $chunkStart, $chunkEnd)
+                                ]
                             ]
-                        ]
-                    );
-                    $chunkStart = $chunkEnd + 1;
-                    fwrite($fp, $response->getBody()->getContents());
+                        );
+                        $chunkStart = $chunkEnd + 1;
+                        fwrite($fp, $response->getBody()->getContents());
+
+                        $percentage = floor($chunkStart / $file->getSize() * 100);
+                        printf("\rDownloading...(%s/%sMB) [%s%s]", round($chunkStart / 1024 / 1024), round($file->getSize() / 1024 / 1024), str_repeat('#', $percentage), str_repeat('-', (100 - $percentage)));
+                    }
+                    printf("\r\nDownload complete.\n");
+
+                    // close the file pointer
+                    fclose($fp);
                 }
 
-                // close the file pointer
-                fclose($fp);
-            }
+                printf("Start transcode.\n");
 
-            printf("Start transcode.\n");
+                Storage::deleteDirectory('transcode/hls');
+                Storage::makeDirectory('transcode/hls');
 
-            Storage::deleteDirectory('transcode/hls');
-            Storage::makeDirectory('transcode/hls');
+                $videoHandle = $ffmpeg->customInput(Storage::path($mp4Path), $usingCuda ? ['-hwaccel', 'cuda'] : []);
 
-            $videoHandle = $ffmpeg->customInput(Storage::path($mp4Path), $usingCuda ? ['-hwaccel', 'cuda'] : []);
+                $format = new X264();
+                $format->on('progress', function ($video, $format, $percentage) {
+                    printf("\rTranscoding...(%s%%) [%s%s]", $percentage, str_repeat('#', $percentage), str_repeat('-', (100 - $percentage)));
+                });
 
-            $format = new X264();
-            $format->on('progress', function ($video, $format, $percentage) {
-                printf("\rTranscoding...(%s%%) [%s%s]", $percentage, str_repeat('#', $percentage), str_repeat('-', (100 - $percentage)));
-            });
+                //A path you want to save a random key to your local machine
+                $saveKeyTo = Storage::path($keyPath);
+                //A URL (or a path) to access the key on your website
+                $keyUrl = route('video.old_key', ['id' => $video->video_id]);
 
-            //A path you want to save a random key to your local machine
-            $saveKeyTo = Storage::path($keyPath);
-            //A URL (or a path) to access the key on your website
-            $keyUrl = route('video.old_key', ['id' => $video->video_id]);
+                $r720p  = (new Representation)->setKiloBitrate(1000)->setResize(1280, 720);
+                $r1080p  = (new Representation)->setKiloBitrate(2000)->setResize(1920, 1080);
 
-            $r720p  = (new Representation)->setKiloBitrate(1000)->setResize(1280, 720);
-            $r1080p  = (new Representation)->setKiloBitrate(2000)->setResize(1920, 1080);
+                $videoHandle
+                    ->hls()
+                    ->encryption($saveKeyTo, $keyUrl)
+                    ->setFormat($format)
+                    ->addRepresentations([$r1080p, $r720p])
+                    ->save(Storage::path($m3u8Path));
 
-            $videoHandle
-                ->hls()
-                ->encryption($saveKeyTo, $keyUrl)
-                ->setFormat($format)
-                ->addRepresentations([$r1080p, $r720p])
-                ->save(Storage::path($m3u8Path));
+                printf("\r\nTrancoded complete.\n");
 
-            printf("\r\nTrancoded complete.\n");
+                printf("Start upload to s3.\n");
 
-            printf("Start upload to s3.\n");
+                while (true) {
+                    Storage::disk('s3')->deleteDirectory('streaming/' . $video->video_id);
 
-            while (true) {
-                Storage::disk('s3')->deleteDirectory('streaming/' . $video->video_id);
+                    try {
+                        printf("Transferring secret key.\n");
+                        $this->uploadFileToS3('transcode/secret.key', 'streaming/' . $video->video_id . '/secret.key');
+                        $this->uploadDirectoryToS3('transcode/hls/', 'streaming/' . $video->video_id . '/hls/', true);
 
-                try {
-                    printf("Transferring secret key.\n");
-                    $this->uploadFileToS3('transcode/secret.key', 'streaming/' . $video->video_id . '/secret.key');
-                    $this->uploadDirectoryToS3('transcode/hls/', 'streaming/' . $video->video_id . '/hls/', true);
-
-                    break;
-                } catch (Exception $ex) {
-                    printf("[ERROR] " . $ex->getMessage() . "\nRe-upload after 5s...\n");
-                    sleep(5);
+                        break;
+                    } catch (Exception $ex) {
+                        printf("[ERROR] " . $ex->getMessage() . "\nRe-upload after 5s...\n");
+                        sleep(5);
+                    }
                 }
+
+                printf("Update stream url to database.\n");
+
+                DB::connection('old_db')->table('video')->where('video_id', $video->video_id)->update([
+                    'stream_url' => Storage::disk('s3')->url('streaming/' . $video->video_id . '/hls/playlist.m3u8')
+                ]);
+
+                printf("Delete transcode directory.\n");
+
+                Storage::deleteDirectory('transcode');
+            } catch (\Throwable $th) {
+                printf("Exception, revert current trancode.\n");
+
+                DB::connection('old_db')->table('video')->where('video_id', $video->video_id)->update([
+                    'stream_url' => null
+                ]);
             }
-
-            printf("Update stream url to database.\n");
-
-            DB::connection('old_db')->table('video')->where('video_id', $video->video_id)->update([
-                'stream_url' => Storage::disk('s3')->url('streaming/' . $video->video_id . '/hls/playlist.m3u8')
-            ]);
-
-            printf("Delete transcode directory.\n");
-
-            Storage::deleteDirectory('transcode');
         }
     }
 
