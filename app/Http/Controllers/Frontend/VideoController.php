8b295c1ade2019e6bc971222ed4e6024dfa50ecd
mrcyclo <vipboysanhdieu@gmail.com>
2020-12-09 20:15:20 +0700

    Cache video key for 1 mins

--- app/Http/Controllers/Frontend/VideoController.php
+++ app/Http/Controllers/Frontend/VideoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Cache;
+use Cookie;
 use Illuminate\Http\Request;
 use Storage;
 
@@ -16,9 +18,14 @@ class VideoController extends Controller
     public function oldGetKey($id)
     {
         \Debugbar::disable();
-        $key = Storage::disk('s3')->get("streaming/$id/secret.key");
-        return response($key)->withHeaders([
-            'Access-Control-Allow-Origin' => 'http://tomatoonline.edu.vn'
-        ]);
+        $key = Cache::get("video-$id");
+        if ($key == null) {
+            $key = Storage::disk('s3')->get("streaming/$id/secret.key");
+        }
+        return response($key)
+            ->withHeaders([
+                'Access-Control-Allow-Origin' => 'http://tomatoonline.edu.vn'
+            ])
+            ->cookie("video-$id", $key, 1);
     }
 }
