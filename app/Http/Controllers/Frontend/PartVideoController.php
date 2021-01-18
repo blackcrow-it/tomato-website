b4c56ea54292066e631c13f183aac95c72b36744
mrcyclo <vipboysanhdieu@gmail.com>
2021-01-18 20:32:05 +0700

    fix cors

--- app/Http/Controllers/Frontend/PartVideoController.php
+++ app/Http/Controllers/Frontend/PartVideoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Part;
 use App\PartVideo;
+use Auth;
 use Illuminate\Http\Request;
 use Storage;
 
@@ -13,6 +14,8 @@ class PartVideoController extends Controller
     public function getKey($id)
     {
         \Debugbar::disable();
+
+        if (!Auth::check()) return response('Unauthorized.', 500);
 
         $part = Part::findOrFail($id);
 
