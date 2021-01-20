d73488b6a3f7d83221164d5919a571b0ee8b05ee
Le Tuan Anh <anhlt2@vnext.com.vn>
2021-01-20 10:30:34 +0700

    Sửa lỗi ko đăng nhập được tk khác sau khi đăng xuất

--- app/Http/Controllers/Backend/LogoutController.php
+++ app/Http/Controllers/Backend/LogoutController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Auth;
 use Illuminate\Http\Request;
+use Session;
 
 class LogoutController extends Controller
 {
     public function logout() {
         Auth::logout();
+        Session::flush();
 
         return redirect()->route('admin.login');
     }
--- app/Http/Controllers/Frontend/LogoutController.php
+++ app/Http/Controllers/Frontend/LogoutController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Auth;
 use Illuminate\Http\Request;
+use Session;
 
 class LogoutController extends Controller
 {
     public function logout() {
         Auth::logout();
+        Session::flush();
 
         return redirect()->route('home');
     }
