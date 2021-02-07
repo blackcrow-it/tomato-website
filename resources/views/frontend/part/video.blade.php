69dd715f7fe15f756d7d90ba9ccd89f3709a3f45
Le Tuan Anh <anhlt2@vnext.com.vn>
2021-02-07 22:09:44 +0700

    Update video.blade.php

--- resources/views/frontend/part/video.blade.php
+++ resources/views/frontend/part/video.blade.php
@@ -1,37 +1,22 @@
 @extends('frontend.part.master')
 
 @section('content')
-<link href="https://vjs.zencdn.net/7.7.6/video-js.css" rel="stylesheet" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/videojs-hls-quality-selector@1.1.1/dist/videojs-hls-quality-selector.css">
-<style>
-    .video-js .vjs-big-play-button {
-        left: 50%;
-        top: 50%;
-        transform: translateX(-50%) translateY(-50%);
-    }
-
-</style>
-
 <div class="learningLesson__video">
-    <div class="product-detail__img">
-        <video id="video" class="video-js" controls preload="auto" data-setup="{}"></video>
+    <div class="embed-responsive embed-responsive-16by9">
+        <video class="embed-responsive-item" id="video" controls preload="auto"></video>
     </div>
 </div>
 
-<script src="https://vjs.zencdn.net/7.7.6/video.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.15.0/videojs-contrib-hls.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/videojs-contrib-quality-levels@2.0.9/dist/videojs-contrib-quality-levels.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/videojs-hls-quality-selector@1.1.1/dist/videojs-hls-quality-selector.min.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 <script>
-    var player = videojs('video');
-    player.src({
-        src: '{{ $stream_url }}',
-        type: 'application/x-mpegURL'
-    });
-    player.aspectRatio('16:9');
-    player.fluid(true);
-    player.hlsQualitySelector();
+    var url = '{{ $stream_url }}';
+
+    if (Hls.isSupported()) {
+        var video = document.getElementById('video');
+        var hls = new Hls();
+        hls.loadSource(url);
+        hls.attachMedia(video);
+    }
 
 </script>
 @endsection
@@ -83,7 +68,8 @@
 @endsection
 
 @section('footer')
-    <script>
-        $('.entry-detail img').css('height', 'auto');
-    </script>
+<script>
+    $('.entry-detail img').css('height', 'auto');
+
+</script>
 @endsection
