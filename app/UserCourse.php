1ada30d7cf5667ca22c2de3f77495443b7edde67
Nguyễn Quang Hưng <quanghungleo@gmail.com>
2021-08-03 02:57:43 +0700

    Add: Command Copy học viên từ khoá học sang khoá học mới

--- app/UserCourse.php
+++ app/UserCourse.php
@@ -22,4 +22,9 @@ class UserCourse extends BaseModel
     {
         return $this->belongsTo('App\Course', 'course_id', 'id');
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
