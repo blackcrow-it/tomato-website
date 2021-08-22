0e663180d161ec7ff1974e3bfe18db1f80cc691c
Nguyễn Quang Hưng <quanghungleo@gmail.com>
2021-08-22 11:39:35 +0700

    Add: Thêm config ảnh nền

--- resources/views/backend/setting/display.blade.php
+++ resources/views/backend/setting/display.blade.php
@@ -19,4 +19,46 @@ Hiển thị
         </div>
     </div>
 </div>
+<div class="row">
+    <div class="col-sm-6">
+        <div class="form-group">
+            <label>Ảnh nền trang đăng nhập</label>
+            <div class="input-group">
+                <input type="text" name="login_background" placeholder="Đăng nhập" :value="getValue('login_background', '{{ config('settings.login_background') }}')" class="form-control">
+                <div class="input-group-append">
+                    <button type="button" class="input-group-text" @click="uploadImage('login_background')">Chọn file</button>
+                </div>
+            </div>
+            <img class="image-preview" :src="getValue('login_background', '{{ config('settings.login_background') }}')">
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6">
+        <div class="form-group">
+            <label>Ảnh nền trang đăng ký</label>
+            <div class="input-group">
+                <input type="text" name="register_background" placeholder="Đăng ký" :value="getValue('register_background', '{{ config('settings.register_background') }}')" class="form-control">
+                <div class="input-group-append">
+                    <button type="button" class="input-group-text" @click="uploadImage('register_background')">Chọn file</button>
+                </div>
+            </div>
+            <img class="image-preview" :src="getValue('register_background', '{{ config('settings.register_background') }}')">
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6">
+        <div class="form-group">
+            <label>Ảnh nền trang quên mật khẩu</label>
+            <div class="input-group">
+                <input type="text" name="forgot_password_background" placeholder="Quên mật khẩu" :value="getValue('forgot_password_background', '{{ config('settings.forgot_password_background') }}')" class="form-control">
+                <div class="input-group-append">
+                    <button type="button" class="input-group-text" @click="uploadImage('forgot_password_background')">Chọn file</button>
+                </div>
+            </div>
+            <img class="image-preview" :src="getValue('forgot_password_background', '{{ config('settings.forgot_password_background') }}')">
+        </div>
+    </div>
+</div>
 @endsection
