ad36181d8fe800242a321e82f73c26671d104849
Nguyễn Quang Hưng <quanghungleo@gmail.com>
2021-08-22 14:29:41 +0700

    Add: Thêm phần config ảnh

--- resources/views/backend/setting/display.blade.php
+++ resources/views/backend/setting/display.blade.php
@@ -8,7 +8,7 @@ Hiển thị
 <div class="row">
     <div class="col-sm-6">
         <div class="form-group">
-            <label>Ảnh nền đăng ký nhận tin</label>
+            <label>Ảnh nền đăng ký nhận tin (832x1013)</label>
             <div class="input-group">
                 <input type="text" name="consultation_background" placeholder="Đăng ký nhận tin" :value="getValue('consultation_background', '{{ config('settings.consultation_background') }}')" class="form-control">
                 <div class="input-group-append">
@@ -22,7 +22,7 @@ Hiển thị
 <div class="row">
     <div class="col-sm-6">
         <div class="form-group">
-            <label>Ảnh nền trang đăng nhập</label>
+            <label>Ảnh nền trang đăng nhập (1110x743)</label>
             <div class="input-group">
                 <input type="text" name="login_background" placeholder="Đăng nhập" :value="getValue('login_background', '{{ config('settings.login_background') }}')" class="form-control">
                 <div class="input-group-append">
@@ -36,7 +36,7 @@ Hiển thị
 <div class="row">
     <div class="col-sm-6">
         <div class="form-group">
-            <label>Ảnh nền trang đăng ký</label>
+            <label>Ảnh nền trang đăng ký (1110x846)</label>
             <div class="input-group">
                 <input type="text" name="register_background" placeholder="Đăng ký" :value="getValue('register_background', '{{ config('settings.register_background') }}')" class="form-control">
                 <div class="input-group-append">
@@ -50,7 +50,7 @@ Hiển thị
 <div class="row">
     <div class="col-sm-6">
         <div class="form-group">
-            <label>Ảnh nền trang quên mật khẩu</label>
+            <label>Ảnh nền trang quên mật khẩu (1110x637)</label>
             <div class="input-group">
                 <input type="text" name="forgot_password_background" placeholder="Quên mật khẩu" :value="getValue('forgot_password_background', '{{ config('settings.forgot_password_background') }}')" class="form-control">
                 <div class="input-group-append">
